e2ab2e1612eee205964b352764a28565955c232e
Devendra Dantal <devendra.dantal04@gmail.com>
2022-09-10 13:39:21 +0530

    Stripe Gateway

--- app/http/controllers/customers/orderController.js
+++ app/http/controllers/customers/orderController.js
@@ -1,5 +1,6 @@
 const Order = require("../../../models/order");
 const moment = require("moment");
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 
 function orderController () {
@@ -7,11 +8,13 @@ function orderController () {
         store(req,res) {
             //Validate Request
 
-            const {phone, address} = req.body;
+            const {phone, address, stripeToken, paymentType} = req.body;
+
 
             if(!phone || !address) {
-                req.flash('error', "All fields are required")
-                return res.redirect('/cart')
+                return res.status(422).json({message : "All fields are required"});
+                // req.flash('error', "All fields are required")
+                // return res.redirect('/cart')
             }
 
             const order = new Order({
@@ -22,19 +25,83 @@ function orderController () {
             })
 
             order.save().then( result => {
-                Order.populate(result, {path : 'customerId'}, (err, data) => {
+                Order.populate(result, {path : 'customerId'}, async (err, data) => {
+
+                    //Stripe payment
+                    if(paymentType === 'card') {
+                        const paymentMethod = await stripe.paymentMethods.create({
+                            type: 'card',
+                            card: {
+                              number: '4000000000003220',
+                              exp_month: 9,
+                              exp_year: 2023,
+                              cvc: '314',
+                            },
+                          });
+                          
+                        stripe.paymentIntents.create({
+                            amount : req.session.cart.totalPrice * 100,
+                            // source : stripeToken,
+                            currency : 'inr',
+                            payment_method_types : ["card"],
+                            payment_method : paymentMethod.id,
+                            statement_descriptor : `Pizza Order Succeful`,
+                            metadata : {
+                                order_id : `${data._id}`,
+                                user_id : `${req.user._id}`
+                            },
+                            description : `Order Id : ${data._id}`,
+                        }).then((payment_intent) => {
+                            data.paymentStatus = true;
+                            data.save().then( async (ord)=> {
+                                
+                                //Emit
+                                const emitter = req.app.get('eventEmitter');
+                                emitter.emit('orderPlaced', ord)
+
+                                delete req.session.cart
+                                
                     
-                    req.flash('success', 'Order placed Successfuly');
-                    delete req.session.cart
-                    //Emit
-                    const emitter = req.app.get('eventEmitter');
-                    emitter.emit('orderPlaced', data)
-                    res.redirect('/customer/orders')
+                            // res.redirect('/customer/orders')
+                                const paymentIntent = await stripe.paymentIntents.confirm(
+                                    payment_intent.id,
+                                    {payment_method: paymentMethod.id}
+                                );
+
+
+                                // const paymentIntentCapture = await stripe.paymentIntents.capture(
+                                //     payment_intent.id
+                                // )
+
+                                return res.json({message : "Payment successful, Successfuly order placed"});
+                            }).catch(err => {
+                                console.log(err)
+                            })
+
+                        }).catch(err => {
+                            console.log("I am here 2")
+                            console.log(err)
+                            return res.json({message : "Payment failed, you can pay at delivery time"});
+                        })
+                    }else {
+                        // Emit
+                            const emitter = req.app.get('eventEmitter');
+                            emitter.emit('orderPlaced', data)
+
+                            delete req.session.cart
+                
+                            // res.redirect('/customer/orders')
+                            return res.json({message : "Successfuly order placed"});
+                    }
+                    
+                            
+                    // req.flash('success', 'Order placed Successfuly');
                 })
 
             }).catch(err => {
-                req.flash('error', 'Something went wrong');
-                return res.redirect('/cart');
+                // req.flash('error', 'Something went wrong');
+                return res.json({message : "Something went wrong"});
+                // return res.redirect('/cart');
             })
         },
         async index(req,res) {
